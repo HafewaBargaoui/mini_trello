ed03c7eb96e3323e7cf2ae1ce12e91b6c9e1d2e7
Hafawa Bargaoui <hafewa.bargaoui@gmail.com>
2025-04-30 11:49:40 +0200

    fix function add_list_to_board

--- lists.py
+++ lists.py
@@ -1,37 +1,33 @@
 import json
+board_parent = {
+    "dict_test" : {
+        "toto": [1,2,3,4],
+        "tata": [1,2,3,4],
+        "tutu": [1,2,3,4],
+},
 
-boards = {
-    "titi": [
-        [1, 2, 3],        
-        [4, 5],            
-    ],
-    "haha": [
-        [7, 8, 9],
-        [10],
-        [10, 11, 0]
-    ],
-    "kiki": [
-        [9, 77, 5],
-        []
-    ]
+    "dict_test2" : {
+        "titi": [1,2,3,4],
+        "tyty": [1,2,3,4],
+        "toto": [1,2,3,4],
 }
-def add_list_to_board(board_dict, board_name, new_list):
- """
-    Ajoute une nouvelle liste (fournie en paramètre) à un board donné.
-     
-    :param board_dict: Le dictionnaire contenant tous les boards.
-    :param board_name: Le nom du board auquel ajouter la liste.
-    :param new_list: La liste à ajouter (ex: [42, 43]).
+}
+def add_list_to_board(board_parent, board_name, key, new_list):
     """
- if board_name in board_dict:
-        board_dict[board_name].append(new_list)  
-        print(f"Nouvelle liste ajoutée au board '{board_name}'.")
- else:
+    Ajoute une nouvelle liste sous la clé `key` dans le board `board_name` 
+    du dictionnaire `board_parent`, si cette clé n'existe pas encore.
+    """
+    if board_name in board_parent:
+        board = board_parent[board_name]
+        if key not in board:
+            board[key] = new_list
+            print(f"Clé '{key}' ajoutée au board '{board_name}' avec la liste {new_list}.")
+        else:
+            print(f"La clé '{key}' existe déjà dans le board '{board_name}'.")
+    else:
         print(f"Le board '{board_name}' n'existe pas.")
 
+#delete_list_from_board(dict_test, "toto")
+add_list_to_board(board_parent, "dict_test2", "test", [1])
 
-
-add_list_to_board(boards, "titi4", [5,777777])
-
-
-print(boards)
+print (board_parent )
